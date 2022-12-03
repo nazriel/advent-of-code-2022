4f577fb847171230585e1077a354f4fcb099a7cb
Damian Ziemba <nazriel@dzfl.pl>
2022-12-03 21:55:09 +0100

    test(day-1): Add unit tests for all routines
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- 01/main.d
+++ 01/main.d
@@ -3,9 +3,24 @@ string validator(string line)
     return "";
 }
 
+///
+unittest
+{
+    assertNotThrown(validator(""));
+    assertNotThrown(validator("1234"));
+    assertThrown(validator("a"));
+    assertThrown(validator("."));
+}
+
 string[] getLines(string fname)
 {
     return [""];
+}
+
+///
+unittest
+{
+    assert(getLines("test_input.txt").length == 14);
 }
 
 uint[] extractElvesCalories(string[] data)
@@ -13,9 +28,28 @@ uint[] extractElvesCalories(string[] data)
     return [];
 }
 
+unittest
+{
+    assert(extractElvesCalories([]) == []);
+
+    assert(extractElvesCalories(["1", "2"]) == [3]);
+    assert(extractElvesCalories(["1", "2", ""]) == [3]);
+
+    assert(extractElvesCalories(["1", "2", "", "4"]) == [3, 4]);
+    assert(extractElvesCalories(["1", "2", "", "4", ""]) == [3, 4]);
+}
+
 size_t findMostJuicedElf(uint[] calories)
 {
     return 0;
+}
+
+unittest
+{
+    assert(findMostJuicedElf([]) == 0);
+    assert(findMostJuicedElf([0]) == 1);
+    assert(findMostJuicedElf([100]) == 1);
+    assert(findMostJuicedElf([100, 1000, 500]) == 2);
 }
 
 uint findMostCalories(uint[] calories)
@@ -23,9 +57,30 @@ uint findMostCalories(uint[] calories)
     return 0;
 }
 
+unittest
+{
+    assert(findMostCalories([]) == 0);
+    assert(findMostCalories([0]) == 0);
+    assert(findMostCalories([100]) == 100);
+    assert(findMostCalories([100, 1000, 500]) == 1000);
+}
+
 void main(string[] args)
 {
     string[] lines = getLines(args.length > 1 ? args[1] : "");
     uint[] calories = extractElvesCalories(lines);
     writeln(findMostCalories(calories));
+}
+
+unittest
+{
+    string[] lines = getLines("test_input.txt");
+    assert(lines.length == 14);
+
+    uint[] calories = extractElvesCalories(lines);
+    assert(calories.length == 5);
+
+    assert(findMostJuicedElf(calories) == 4);
+
+    assert(findMostCalories(calories) == 24_000);
 }
