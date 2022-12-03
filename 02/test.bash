0f281c6b76beef28d57da021d0a2d24a0e23d15d
Damian Ziemba <nazriel@dzfl.pl>
2022-12-04 00:09:18 +0100

    test(day-2): Add test case for solution() impl and update test condition for loop()
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- 02/test.bash
+++ 02/test.bash
@@ -4,7 +4,18 @@
 
 bats_require_minimum_version "1.8.0"
 
+function test_solution() { #@test
+  run solution "A Y"
+  [ "$output" -eq 8 ]
+
+  run solution "B X"
+  [ "$output" -eq 1 ]
+
+   run solution "C Z"
+  [ "$output" -eq 6 ]
+}
+
 function test_loop() { #@test
   run loop test_input.txt
-  [ "$status" -eq 0 ]
+  [ "$output" -eq 15 ]
 }
