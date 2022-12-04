f34929f93df63c7755e6eb80516e3dd2e3e7bc56
Damian Ziemba <nazriel@dzfl.pl>
2022-12-04 03:45:21 +0100

    feat(day-3): Check program arguments for input file for processing.
    Handle cases where it is not available.
    Stub for get_lines() routine
    
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- 03/main.rs
+++ 03/main.rs
@@ -1,3 +1,21 @@
+use std::env::args as cli_args;
+use std::path::Path;
+
+fn get_lines(filename: &Path) -> Vec<String> {
+    vec![""]
+}
+
 fn main() {
-    println!("total: {:?}", 0)
+    let args: Vec<String> = cli_args().collect();
+    if args.len() != 2 {
+        panic!("input file not provided. usage: {:?} input_file.txt", args[0])
+    }
+
+    let input_file = Path::new(&args[1]);
+    if !input_file.exists() {
+        panic!("provided input file doesn't exist: {:?}", input_file.to_str().unwrap());
+    }
+
+    let lines = get_lines(input_file);
+    println!("total: {:?}", lines.len())
 }
