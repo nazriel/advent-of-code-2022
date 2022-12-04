c45011b5e97dfd7f3501f186c7ba16dd68e94397
Damian Ziemba <nazriel@dzfl.pl>
2022-12-04 12:14:19 +0100

    feat(day-4): program flow with stubs
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- 04/src/main.c
+++ 04/src/main.c
@@ -1,8 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "main.h"
 
+struct Line* get_lines(const char* filename) {
+    return NULL;
+}
+
+struct Pair* extract_pairs(struct Line* lines) {
+    return NULL;
+}
+
+unsigned int count_overlapping_pairs(struct Pair* pairs) {
+    unsigned int overlaping = 0;
+    return overlaping;
+}
+
 int main(int argc, const char** argv) {
-    (void) argc;
-    (void) argv;
-    printf("overlaping pairs: %d\n", 0);
+    struct Line* lines = get_lines(argv[1]);
+    struct Pair* pairs = extract_pairs(lines);
+    printf("overlaping pairs: %d\n", count_overlapping_pairs(pairs));
     return 0;
 }
