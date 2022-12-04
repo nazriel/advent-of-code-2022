fe568ff5ee4c971ff636a12a60f386201f0976bc
Damian Ziemba <nazriel@dzfl.pl>
2022-12-04 03:54:55 +0100

    feat(day-3): calculate weight of duplicate items in given backpack
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- 03/main.rs
+++ 03/main.rs
@@ -12,6 +12,16 @@ fn get_lines(filename: &Path) -> Vec<String> {
         .collect()
 }
 
+fn item_prio(item: char) -> u32 {
+    if item >= 'a' && item <= 'z' {
+        item as u32 - 96
+    } else if item >= 'A' && item <= 'Z' {
+        item as u32 - 38
+    } else {
+        0
+    }
+}
+
 fn split_backpack(equipment: &str) -> [&str; 2] {
     let middle = equipment.len() / 2;
     [&equipment[..middle], &equipment[middle..]]
@@ -31,6 +41,13 @@ fn find_mixed_items(left: &str, right: &str) -> Vec<char> {
     res
 }
 
+fn backpack_duplicates_weight(mixed_items: &Vec<char>) -> u32 {
+    mixed_items
+        .iter()
+        .map(| item | item_prio(*item))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,6 +59,19 @@ mod tests {
             "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"
         ];
         assert_eq!(get_lines(Path::new("test_input.txt")), expected);
+    }
+
+    #[test]
+    fn test_item_prio() {
+        assert_eq!(item_prio(' '), 0);
+        assert_eq!(item_prio('9'), 0);
+        assert_eq!(item_prio('a'), 1);
+        assert_eq!(item_prio('A'), 27);
+        assert_eq!(item_prio('p'), 16);
+        assert_eq!(item_prio('P'), 42);
+        assert_eq!(item_prio('L'), 38);
+        assert_eq!(item_prio('z'), 26);
+        assert_eq!(item_prio('Z'), 52);
     }
 
     #[test]
@@ -61,6 +91,16 @@ mod tests {
         assert_eq!(find_mixed_items("abc", "abc"), ['a', 'b', 'c']);
         assert_eq!(find_mixed_items("abccdefg", "ihjlac"), ['a', 'c']);
     }
+
+    #[test]
+    fn test_backpack_duplicates_weight() {
+        assert_eq!(backpack_duplicates_weight(&vec![]), 0);
+        assert_eq!(backpack_duplicates_weight(&vec!['1']), 0);
+        assert_eq!(backpack_duplicates_weight(&vec!['?']), 0);
+        assert_eq!(backpack_duplicates_weight(&vec!['a']), 1);
+        assert_eq!(backpack_duplicates_weight(&vec!['a', 'b', 'c']), 6);
+        assert_eq!(backpack_duplicates_weight(&vec!['A', 'Z']), 27 + 52);
+    }
 }
 
 fn main() {
@@ -77,7 +117,8 @@ fn main() {
     let lines = get_lines(input_file);
     for line in lines {
         let [left, right] = split_backpack(&line);
-        let _ = find_mixed_items(left, right);
+        let mixed_items = find_mixed_items(left, right);
+        let _: u32 = backpack_duplicates_weight(&mixed_items);
     }
     println!("total: {:?}", 0)
 }
